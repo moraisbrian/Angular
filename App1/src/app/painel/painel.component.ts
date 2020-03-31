451a3d68341bd3d074d8a2b97ffd33310f51bcc3
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-03-31 13:36:19 -0300

    Aula 83 finalizada

--- App1/src/app/painel/painel.component.ts
+++ App1/src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Frase } from "../shared/frase.model";
 import { FRASES } from "./frases-mock";
 
@@ -20,6 +20,8 @@ export class PainelComponent implements OnInit {
 
     public tentativas: number = 3;
 
+    @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter();
+
     constructor() { 
         this.atualizaRodada();
     }
@@ -33,14 +35,19 @@ export class PainelComponent implements OnInit {
 
     public verificaResposta(): void {
         if (this.rodadaFrase.frasePtBr == this.resposta) {
-            alert("A tradução está correta!");
             this.rodada++;
-            this.atualizaRodada();
-            this.progresso += 25;
+            
+            this.progresso += (100 / this.frases.length);
+
+            if (this.rodada == 4) {
+                this.encerrarJogo.emit("vitoria");
+            }
+            
+            this.atualizaRodada()
         } else {
             this.tentativas--;
             if (this.tentativas == -1) {
-                alert("Você perdeu todas as tentativas!");
+                this.encerrarJogo.emit("derrota");
             }
         }
         
