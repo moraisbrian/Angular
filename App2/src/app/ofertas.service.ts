124b807c664d164d917a41a0b4687bf42638096e
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-04-02 17:13:15 -0300

    Aula 135 finalizada

--- App2/src/app/ofertas.service.ts
+++ App2/src/app/ofertas.service.ts
@@ -9,19 +9,31 @@ export class OfertasService {
     constructor(private http: HttpClient){ }
 
     public getOfertas(): Promise<Oferta[]> {
-        return this.http.get(`${URL_API}?destaque=true`)
+        return this.http.get(`${URL_API}/ofertas?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta);
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.http.get(`${URL_API}?categoria=${categoria}`)
+        return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any) => resposta);
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
-        return this.http.get(`${URL_API}?id=${id}`)
+        return this.http.get(`${URL_API}/ofertas?id=${id}`)
+            .toPromise()
+            .then((resposta: any) => resposta);
+    }
+
+    public getComoUsarOfertaPorId(id: number): Promise<string> {
+        return this.http.get(`${URL_API}/como-usar?id=${id}`)
+            .toPromise()
+            .then((resposta: any) => resposta);
+    }
+
+    public getOndeFicaPorId(id: number): Promise<string> {
+        return this.http.get(`${URL_API}/onde-fica?id=${id}`)
             .toPromise()
             .then((resposta: any) => resposta);
     }
