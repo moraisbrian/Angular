befd0c7fabccb9f71375e2ac867e5e693aa7b5da
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-04-02 11:33:35 -0300

    Aula 117 finalizada

--- App2/src/app/restaurantes/restaurantes.component.ts
+++ App2/src/app/restaurantes/restaurantes.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from "../shared/oferta.model";
+import { OfertasService } from "../ofertas.service";
 
 @Component({
-  selector: 'app-restaurantes',
-  templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css']
+    selector: 'app-restaurantes',
+    templateUrl: './restaurantes.component.html',
+    styleUrls: ['./restaurantes.component.css'],
+    providers: [OfertasService]
 })
 export class RestaurantesComponent implements OnInit {
 
-  constructor() { }
+    public ofertas: Array<Oferta>;
 
-  ngOnInit(): void {
-  }
+    constructor(private ofertasService: OfertasService) { }
+
+    ngOnInit(): void {
+        this.ofertasService.getOfertasPorCategoria("restaurante")
+            .then((ofertas: Oferta[]) => {
+                this.ofertas = ofertas;
+            });
+    }
 
 }
