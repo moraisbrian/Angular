1b78dd61c89c3fe2cd8684c8149270fbc9fc79c8
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-04-03 12:11:11 -0300

    Aula 142 finalizada

--- App2/src/app/oferta/oferta.component.ts
+++ App2/src/app/oferta/oferta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from "../ofertas.service";
 import { Oferta } from '../shared/oferta.model';
+import { Observable, interval, Observer } from 'rxjs';
 
 @Component({
     selector: 'app-oferta',
@@ -25,6 +26,34 @@ export class OfertaComponent implements OnInit {
                 this.oferta = oferta[0];
                 this.imagemDestaque = oferta[0].imagens[0].url;
             });
+
+        //Observable (observável)
+        let meuObservableTeste = Observable.create((observer: Observer<string>) => {
+            observer.next("Primeiro evento do stream");
+            observer.next("Segundo evento do stream");
+            observer.complete();
+            observer.error("Erro no evento");
+        });
+
+        //Observable (observador)
+        meuObservableTeste.subscribe(
+            (resultado: any) => console.log(resultado), // next()
+            (erro: string) => console.log(erro), // error()
+            () => console.log("Stream foi finalizada") // complete()
+        );
+
+        /*
+        let tempo = interval(500);
+        tempo.subscribe((intervalo: number) => {
+            console.log(intervalo);
+        })
+
+        this.route.params.subscribe(
+            (parametro: any) => console.log(parametro),
+            (erro: any) => console.log(erro),
+            () => console.log("Processamento foi classificado como concluído!")
+        );
+        */
     }
 
 }
