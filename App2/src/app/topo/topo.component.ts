547d5f0d950ed1089a7d0b833bf50ef38e6b2f4a
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-04-06 17:48:51 -0300

    Aula 157 finalizada

--- App2/src/app/topo/topo.component.ts
+++ App2/src/app/topo/topo.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
 import { OfertasService } from "../ofertas.service";
 import { Observable, Subject, of } from 'rxjs';
 import { Oferta } from '../shared/oferta.model';
-import { switchMap, debounceTime } from "rxjs/operators";
+import { switchMap, debounceTime, distinctUntilChanged, catchError } from "rxjs/operators";
 
 @Component({
     selector: 'app-topo',
@@ -21,11 +21,16 @@ export class TopoComponent implements OnInit {
         this.ofertas = this.subjectPesquisa
             .pipe(
                 debounceTime(1000), // Tempo de execução do switchMap em milisegundos
+                distinctUntilChanged(), // Verifica se o ultimo parâmetro é igual ao atual
                 switchMap((termo: string) => {
                     if (termo.trim() == "") {
                         return of<Oferta[]>([]);
                     }
                     return this.ofertasService.pesquisaOfertas(termo);
+                }),
+                catchError((erro: any) => {
+                    console.log("Erro do subject: ", erro);
+                    return of<Oferta[]>([]);
                 })
             );
         
